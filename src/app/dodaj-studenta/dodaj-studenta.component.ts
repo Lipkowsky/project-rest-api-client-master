7e8c0f73594321338cfdfea5c9e737508bbbb42b
Lipkowsky <piotr.lipkowsky@gmail.com>
2021-07-04 17:28:01 +0200

    new student

--- src/app/dodaj-studenta/dodaj-studenta.component.ts
+++ src/app/dodaj-studenta/dodaj-studenta.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,23 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dodaj-studenta.component.css']
 })
 export class DodajStudentaComponent implements OnInit {
+  url = 'http://localhost:8080/api/';
+
   imie!: string;
   nazwisko!: string;
   nrIndeksu!: number;
   email!: string;
-  stacjonarny!: boolean;
+  stacjonarny: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
   dodajStudenta(){
-    console.log(this.imie);
-    console.log(this.nazwisko);
-    console.log(this.nrIndeksu);
-    console.log(this.email);
-    console.log(this.stacjonarny);
+    let options = {};
+    const student = {
+      imie: this.imie,
+      nazwisko:this.nazwisko,
+      nr_indeksu: this.nrIndeksu,
+      email:this.email,
+      stacjonarny:this.stacjonarny
+    }
+
+    console.log(student);
+    const result = this.http.post<any>(this.url + 'nowyStudent?imie=' + student.imie + '&nazwisko=' + this.nazwisko + '&nr_indeksu=' + student.nr_indeksu + '&email=' + student.email + '&stacjonarny=' + student.stacjonarny, {options});
+    result.subscribe(data => {
+      console.log(data);
+    })
   }
+
+
 
 }
